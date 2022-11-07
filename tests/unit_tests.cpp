1e4408304463303d1b52bc7f8db57404fcbf272e
Patrick Hlustik <patrick.hlustik@gmail.com>
2022-11-07 22:14:37 +0100

    add basic gtest setup

---
+++ tests/unit_tests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "../src/Commands/Commands.h"
+
+TEST(HelloTest, BasicAssertions) {
+  SetCWD("");
+
+  EXPECT_STRNE("hello", "world");
+  EXPECT_EQ(7 * 6, 42);
+}
